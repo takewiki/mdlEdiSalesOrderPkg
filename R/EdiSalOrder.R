637ce2a066a893e249412b0fbaf46332954e96ee
takewiki <hulilei@takeiwki.com.cn>
2026-06-29 09:28:44 +0000

    v1.0.3

--- R/EdiSalOrder.R
+++ R/EdiSalOrder.R
@@ -22,3 +22,31 @@ EdiSalOrder_view <- function(erp_token,FBillNO) {
   return(res)
 
 }
+
+
+
+#' EDI销售订单删除
+#'
+#' @param erp_token
+#' @param FBillNO
+#'
+#' @return 两个数的和
+#' @export
+#'
+#' @examples
+#' EdiSalOrder_delete()
+EdiSalOrder_delete <- function(erp_token,FBillNO) {
+
+
+  sql=paste0("
+             exec rds_proc_Edi_GetOrderNo_delete '",FBillNO,"'
+             ")
+
+  res = tsda::sql_delete2(token =erp_token ,sql_str = sql)
+
+
+
+  return(res)
+
+}
+
